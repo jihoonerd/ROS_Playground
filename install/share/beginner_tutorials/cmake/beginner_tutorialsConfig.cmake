eaf5bcdff2d87f5366329e039301fa4827f34330
Jihoon Kim <jihoonerd@outlook.com>
2020-12-30 14:42:09 +0900

    Tutorial materials (~8)

--- install/share/beginner_tutorials/cmake/beginner_tutorialsConfig.cmake
+++ install/share/beginner_tutorials/cmake/beginner_tutorialsConfig.cmake
@@ -66,7 +66,7 @@ endif()
 set(beginner_tutorials_CONFIG_INCLUDED TRUE)
 
 # set variables for source/devel/install prefixes
-if("TRUE" STREQUAL "TRUE")
+if("FALSE" STREQUAL "TRUE")
   set(beginner_tutorials_SOURCE_PREFIX /home/jihoon/ROS_Playground/src/beginner_tutorials)
   set(beginner_tutorials_DEVEL_PREFIX /home/jihoon/ROS_Playground/devel)
   set(beginner_tutorials_INSTALL_PREFIX "")
@@ -110,7 +110,7 @@ if(NOT " " STREQUAL " ")
         message(FATAL_ERROR "Project 'beginner_tutorials' specifies '${idir}' as an include dir, which is not found.  It does not exist in '${include}'.  ${_report}")
       endif()
     else()
-      message(FATAL_ERROR "Project 'beginner_tutorials' specifies '${idir}' as an include dir, which is not found.  It does neither exist as an absolute directory nor in '/home/jihoon/ROS_Playground/src/beginner_tutorials/${idir}'.  ${_report}")
+      message(FATAL_ERROR "Project 'beginner_tutorials' specifies '${idir}' as an include dir, which is not found.  It does neither exist as an absolute directory nor in '\${prefix}/${idir}'.  ${_report}")
     endif()
     _list_append_unique(beginner_tutorials_INCLUDE_DIRS ${include})
   endforeach()
@@ -154,7 +154,7 @@ foreach(library ${libraries})
     set(lib_path "")
     set(lib "${library}-NOTFOUND")
     # since the path where the library is found is returned we have to iterate over the paths manually
-    foreach(path /home/jihoon/ROS_Playground/devel/lib;/home/jihoon/ROS_Playground/devel/lib;/opt/ros/noetic/lib)
+    foreach(path /home/jihoon/ROS_Playground/install/lib;/home/jihoon/ROS_Playground/devel/lib;/opt/ros/noetic/lib)
       find_library(lib ${library}
         PATHS ${path}
         NO_DEFAULT_PATH NO_CMAKE_FIND_ROOT_PATH)
